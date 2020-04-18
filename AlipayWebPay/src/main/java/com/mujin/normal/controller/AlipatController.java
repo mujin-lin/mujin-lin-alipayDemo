e93ba220363f3fe2837e135f9401511a6f01c528
mujin-lin <852505232@qq.com>
2020-04-18 16:32:35 +0800

    thmeleaf的引入提交到github

--- AlipayWebPay/src/main/java/com/mujin/normal/controller/AlipatController.java
+++ AlipayWebPay/src/main/java/com/mujin/normal/controller/AlipatController.java
@@ -1,18 +1,20 @@
 package com.mujin.normal.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/alipay")
-@RestController
+@Controller
+@Slf4j
 public class AlipatController {
     /**
      * 查看状态
      * @return
      */
-    @GetMapping("/ping")
+    @RequestMapping("/ping")
     public String ping(){
-        return "ping";
+        log.info("进入ping的方法");
+        return "index";
     }
 }
